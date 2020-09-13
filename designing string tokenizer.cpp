8e75cd4415cd4da7fb42b84940495581335fdbc6
Avinash Kumar <avi.sanu05@gmail.com>
2020-09-13 19:58:30 +0530

    Add files via upload

---
+++ designing string tokenizer.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char *mystrtok(char *str, char delim)
+{
+	static char*input = NULL;
+	if(str != NULL)
+	{
+		input = str;
+	}
+	//base case after the final token has been returned.
+	if(input == NULL)
+	{
+		return NULL;
+	}
+	char *output = new char[strlen(input) + 1];
+	int i=0;
+	for(; input[i]!='\0';i++)
+	{
+		if(input[i] != delim)
+		{
+			output[i] = input[i];
+		}
+		else
+		{
+			output[i] = '\0';
+			input = input + i + 1;
+			return output;
+		}
+	}
+	//corner case
+	output[i] = '\0';
+	input = NULL;
+	return output;
+}
+
+
+int main()
+{
+	char s[100] = "Today, is a rainy day";
+	//strtok stores a static array which stores previously passed string if new string is not provided in next function call
+	char *ptr = mystrtok(s, ' ');
+	cout<<ptr<<endl;
+
+	while(ptr != NULL)
+	{
+		ptr = mystrtok(NULL, ' ');
+		cout<<ptr<<endl;
+	}
+
+	return 0;
+}
